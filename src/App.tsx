3ec1f9ad7214387760ccfe5b92f092dc5e5bd1b3
Sofia <sofia.dionisio94@gmail.com>
2020-12-23 15:42:20 +0000

    Created main functions in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,28 @@
 import React from 'react';
+import QuestionCard from './components/QuestionCard';
 
 function App() {
+
+  const startTrivia = () => {
+
+  };
+
+  const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {
+
+  };
+
+  const nextQuestion = () => {
+
+  };
+
   return (
     <div className="App">
-      The Quiz
-      
+      <h2>The Quiz</h2>
+      <button className="start" onClick={startTrivia}>Start</button>
+      <p>Score: </p>
+      <p>Loading Questions...</p>
+      <QuestionCard />
+      <button className="next" onClick={nextQuestion}>Next Question</button>
     </div>
   );
 }
