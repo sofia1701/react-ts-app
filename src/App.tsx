4c09d03810cd772f93bcadcc991b475d92a51646
Sofia <sofia.dionisio94@gmail.com>
2020-12-23 16:43:34 +0000

    Created initial states in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import QuestionCard from './components/QuestionCard';
 
 function App() {
+	const TOTAL_QUESTIONS = 10;
 
-  const startTrivia = () => {
+	const [loading, setLoading] = useState(false);
+	const [questions, setQuestions] = useState([]);
+	const [number, setNumber] = useState(0);
+	const [userAnswers, setUserAnswers] = useState([]);
+	const [score, setScore] = useState(0);
+	const [gameOver, setGameOver] = useState(true);
 
-  };
+	const startTrivia = async () => {};
 
-  const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {
+	const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {};
 
-  };
+	const nextQuestion = () => {};
 
-  const nextQuestion = () => {
-
-  };
-
-  return (
-    <div className="App">
-      <h2>The Quiz</h2>
-      <button className="start" onClick={startTrivia}>Start</button>
-      <p>Score: </p>
-      <p>Loading Questions...</p>
-      <QuestionCard />
-      <button className="next" onClick={nextQuestion}>Next Question</button>
-    </div>
-  );
+	return (
+		<div className="App">
+			<h2>The Quiz</h2>
+			<button className="start" onClick={startTrivia}>
+				Start
+			</button>
+			<p>Score: </p>
+			<p>Loading Questions...</p>
+			<QuestionCard
+				questionNr={number + 1}
+				question={questions[number].question}
+				answers={questions[number].answers}
+				callback={checkAnswer}
+				userAnswer={userAnswers ? userAnswers[number] : undefined}
+				totalQuestions={TOTAL_QUESTIONS}
+			/>
+			<button className="next" onClick={nextQuestion}>
+				Next Question
+			</button>
+		</div>
+	);
 }
 
 export default App;
