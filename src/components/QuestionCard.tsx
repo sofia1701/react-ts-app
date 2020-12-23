4cd850780c9d3a9f3aa908c8daf03c56207108c6
Sofia <sofia.dionisio94@gmail.com>
2020-12-23 16:01:08 +0000

    Created QuestionCard component

--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -1,9 +1,30 @@
 import React from 'react';
 
-export default function QuestionCard() {
+type Props = {
+  question: string;
+  answers: string[];
+  callback: any;
+  userAnswer: any;
+  questionNr: number;
+  totalQuestions: number;
+}
+
+const QuestionCard: React.FC<Props> = ({question, answers, callback, userAnswer, questionNr, totalQuestions}) => {
   return (
     <div>
-      Question Card
+      <p className="number">Question: {questionNr} / {totalQuestions}</p>
+      <p dangerouslySetInnerHTML={{__html: question}}/>
+      <div>
+        {answers.map(answer => (
+          <div>
+            <button disabled={userAnswer} onClick={callback}>
+              <span dangerouslySetInnerHTML={{__html: answer}}/>
+            </button>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
+
+export default QuestionCard;
