7283be5db248277bd7a92fb171ddb5f610f2d471
Sofia <sofia.dionisio94@gmail.com>
2020-12-27 14:12:11 +0000

    Set state types

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,22 @@
 import React, { useState } from 'react';
 //import QuestionCard from './components/QuestionCard';
 import {FETCH_QUESTIONS} from './API';
-import {Difficulty} from './API';
+import {Difficulty, QuestionState} from './API';
+
+type AnswerObject = {
+  question: string,
+  answer: string,
+  correct: boolean,
+  correctAnswer: string,
+};
 
 function App() {
 	const TOTAL_QUESTIONS = 10;
 
 	const [loading, setLoading] = useState(false);
-	const [questions, setQuestions] = useState([]);
+	const [questions, setQuestions] = useState<QuestionState[]>([]);
 	const [number, setNumber] = useState(0);
-	const [userAnswers, setUserAnswers] = useState([]);
+	const [userAnswers, setUserAnswers] = useState<AnswerObject[]>([]);
 	const [score, setScore] = useState(0);
   const [gameOver, setGameOver] = useState(true);
   
